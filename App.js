78dfce167b7b408b71f88c92dee252d8a57f0df3
espectrozzz <34382426+espectrozzz@users.noreply.github.com>
2022-03-15 10:42:26 -0600

    Actualización Firebase
    Se actualiza el uso de la librería firebase de web version 8 a web version 9

--- App.js
+++ App.js
@@ -1,43 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import firebase, { initializeApp } from "firebase/app"
-import 'firebase/firestore';
-import 'firebase/functions';
-import 'firebase/app';
-// import auth from '@react-native-firebase/app';
-
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
-import { auth } from '../SiosAppMobile/database/firebase';
+import { auth } from './database/firebase';
 import { useNavigation } from '@react-navigation/native';
 
-// const firestore = getFirestore();
-// const navigation = useNavigation()
 
 export default function App() {
 
-  // useEffect(() => {
-  //   const unsubscribe = auth.onAuthStateChanged(user => {
-  //     if(user){
-  //       navigation.navigate("Home")
-  //     }
-  //   })
-  //   return unsubscribe
-  // }, [])
-
   const iniciarSesion = () => {
-      // functions.useFunctionsEmulator("http://localhost:4000");
-    // const auth = fb.auth().useEmulator("http://localhost:9099/");
-    auth.signInWithEmailAndPassword('adpanfilo@ioscomunicaciones.com', '1234567')
-    .then(userCredentials => {
-      const user = userCredentials.user;
-      console.log(user.email);
-    })
-    .catch(error => alert(error.message))
-    console.log('subscriber');
+    signInWithEmailAndPassword(auth, 'test@test.com', '123456')
+      .then((userCredentials) => {
+        console.log("login");
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
   
-
-
   return (
     <View style={styles.container}>
       <TouchableOpacity
